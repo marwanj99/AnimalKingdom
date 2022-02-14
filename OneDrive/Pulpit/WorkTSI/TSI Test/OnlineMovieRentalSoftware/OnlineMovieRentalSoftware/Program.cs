a6895efd940ad03012c1ef7a0648946132f881e3
marwan Al-Maghrabi <marwanjamal7@gmail.com>
2022-02-14 09:18:12 +0000

    Create a rental software

---
+++ OneDrive/Pulpit/WorkTSI/TSI Test/OnlineMovieRentalSoftware/OnlineMovieRentalSoftware/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace OnlineMovieRentalSoftware
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("who are you?");
+            switch (Console.ReadLine() == "Admin") 
+            {
+                case "Customer":
+
+                break;
+                case "Admin":
+
+                break;
+                default:
+                Console.WriteLine("Please state your customer or Aadmin status");
+                break;
+            }
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
